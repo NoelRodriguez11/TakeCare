838283ad84d551f5bc74357afdf87f007cac95bb
NoelRodriguez11 <noelrodriguez2a@gmail.com>
2020-09-09 11:14:45 +0200

    Cambio actual

--- application/views/_templates/head.php
+++ application/views/_templates/head.php
@@ -12,7 +12,7 @@
 	<!-- <script src="<?=base_url()?>assets/js/popper.min.js" ></script>  -->
 	<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 	
-	<title>TakeCare Noel</title>
+	<title>TakeCare Noel2</title>
 </head>
 
 <body>
