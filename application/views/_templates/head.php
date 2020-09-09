b7f868bdc1fc2b126cbcd116438110a309dadcdd
lauraa09 <lauraa09>
2020-09-09 11:17:07 +0200

    cambio Laura

--- application/views/_templates/head.php
+++ application/views/_templates/head.php
@@ -12,7 +12,7 @@
 	<!-- <script src="<?=base_url()?>assets/js/popper.min.js" ></script>  -->
 	<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 	
-	<title>TakeCare Noel2</title>
+	<title>TakeCare TFG</title>
 </head>
 
 <body>
