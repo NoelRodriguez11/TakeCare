e816baa1be32a5111106c55a29a6eaf7b8c96da7
Victor Rivera <victor.rivera4@gmail.com>
2020-09-13 10:44:04 +0200

    Cambio configuración Rb para Heroku

--- application/libraries/Rb.php
+++ application/libraries/Rb.php
@@ -6,7 +6,7 @@ class Rb
     function __construct()
     {
         include (APPPATH . '/config/database.php');
-        include (APPPATH . '/third_party/rb/rb.php');
+        include (APPPATH . '/third_party/rb/Rb.php');
         $host = $db[$active_group]['hostname'];
         $user = $db[$active_group]['username'];
         $pass = $db[$active_group]['password'];
