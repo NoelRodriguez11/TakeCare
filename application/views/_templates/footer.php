30fd124a55d3a2b5243dce28a8f32277c5924313
NoelRodriguez11 <noelrodriguez2a@gmail.com>
2020-09-16 19:34:48 +0200

    Pull con Footer

---
+++ application/views/_templates/footer.php
@@ -0,0 +1,30 @@
+<footer class="container row">
+	<div class="row col-8">
+		<div class="col-4 categoriaEmpresa">
+		<h5>Empresa</h5>
+		<ul>
+				<li><a class="empresa" href="/contacto">Contactanos</a></li>
+				<li><a class="empresa" href="/conocenos">Conócenos</a></li>
+				<li><a class="empresa" href="/info">Información Legal</a></li>
+		</ul>	
+		</div>
+				
+		<div class="col-4 categoriaEspecialidades">
+		<h5>Especialidades</h5>
+			<ul>
+				<li><a class="clinicas" href="/podologia">Podología</a></li>
+				<li><a class="clinicas" href="/odontologia">Odontología</a></li>
+				<li><a class="clinicas" href="/oftalmología">Oftalmología</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="col-4">
+	<h5 class="tituloRedes">Siguenos en las redes sociales</h5>
+		<p class="RRSS">
+		<a href="https://www.instagram.com/cadenadefavores2020"><img src="<?=base_url()?>/assets/img/redes/instagram.png" height="20" width="20"></a>
+		<a href="https://www.facebook.com/cadena.defavores.370177"><img src="<?=base_url()?>/assets/img/redes/facebook.png" height="20" width="20"></a>
+		<a href="https://twitter.com/Cadenad68253986"><img src="<?=base_url()?>/assets/img/redes/twitter.png" height="20" width="20"></a></p>
+	</div>
+		<strong></strong><p class="copyright"><strong>2020-2020 TakeCare Company. <a href="/privacidad"><font face="Comic Sans MS,arial,verdana" size="1" color="teal">Políticas de privacidad y </font></a><a href="/cookies"><font face="Comic Sans MS,arial,verdana" size="1" color="teal">Políticas de cookies</font></a>. </strong></p>
+</footer>
