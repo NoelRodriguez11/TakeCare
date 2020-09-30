00d0a4d807f333ac7d5114f849f1e4164ce25dc3
NoelRodriguez11 <noelrodriguez2a@gmail.com>
2020-09-30 20:02:49 +0200

    Pagina inicial casi terminada

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,20 +1,67 @@
-<div class="container" id="divContenido">
-<div class="contenidoPrincipal">
 	<?php if ($rol == 'auth'):?>
-    <h2>Bienvenido <b><?= $_SESSION['persona']->loginname?></b></h2>
-    	<br>
-    	<a href="<?=base_url()?>carrito/add">
-    		<button>Comprar Productos</button>
-    	</a><br>
-    	<br/>
+	
+	
+	
 	<?php elseif ($rol == 'admin'): ?>
-	<!--  PRUEBA CENTRAR ELEMENTOS-->
-	<a href="<?=base_url()?>carrito/add">
-    		
-    	</a><br>
-    	<code>AQUI IRIA EL CONTENIDO PRINCIPAL</code>
+	
+    	
     	
 	<?php else: ?>
-	<?php endif;?>
+<div class="container" id="divContenido">
+	<div class="contenidoPrincipal">
+		<div class="col-lg-6" id="buscador">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search for...">
+					<span class="input-group-btn">
+						<button class="btn btn-default" type="button">Go!</button>
+					</span>
+			</div>
+		</div>
 	</div>
 </div>
+
+<div class="acerca">
+    <h2>Contacta a los mejores profesionales en tu ciudad</h2>
+    
+    <div class="contenidoExplicativo">
+                    
+        <div class="primeraExplicacion">
+            <span class="logo1">LOGO1</span>
+            <strong><a class="textoexp1" href="">Profesores particulares</a></strong>
+            <br> 
+            <span class="textoexp2">anuncios de profesores particulares que ofrecen clases particulares en su casa o a domicilio.</span>
+        </div>
+        
+        <br>
+        
+        <div class="segundaExplicacion">
+            <span class="logo2">LOGO2</span>
+            <a class="textoexp1" href=""><strong>Clases a domicilio</strong></a> 
+            <br> 
+            <span class="textoexp2">encuentra el profesor particular ideal para recibir clases particulares a domicilio, cómodamente sin salir de casa.</span>
+        </div>
+        
+        <br>
+        
+        <div class="terceraExplicacion">
+            <span class="logo3">LOGO3</span>
+            <a class="textoexp1" href="">Academias</a>
+            <br>
+            <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
+        </div>
+        
+        <br>
+        
+        <div class="cuartaExplicacion">
+            <span class="logo4">LOGO4</span>
+            <a class="textoexp1" href="">Clases para empresas</a>
+            <br> 
+            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
+        </div>
+
+     </div>
+</div>
+                  
+                 
+	<?php endif;?>
+
