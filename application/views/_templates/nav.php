520367f5feb01a2b71d590001e59e61eebdced18
lauraa09 <lauraa09>
2020-10-12 21:12:06 +0200

    arreglado vista menu login

--- application/views/_templates/nav.php
+++ application/views/_templates/nav.php
@@ -1,48 +1,4 @@
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    <a   class="navbar-left" href="<?=base_url()?>"> 
-    <span><img src="<?=base_url()?>assets/img/iconotc.png" alt="INICIO" style="width: 50px; height:50px;"></span>
- 	</a>
-  
-   <a class="navbar-brand" href="<?=base_url()?>"> 
-    <span class="logoTakeCare textoLogo">TakeCare</span>
-  </a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a class="textoexp2" href="#">Diagnóstico Online</a></li>
-        <li><a class="textoexp2" href="<?=base_url()?>profesional/r">Profesionales</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a><button type="button" onclick="cambiarModo()">
-        	<span><i class="fas fa-lightbulb"></i></span>
-    	</button></a>
-    	</li>
-    	
-		
-         <?php if ($datosGen['persona']!=null):?>
-        	<li><a class="textoexp1">Bienvenid@ <?=$datosGen['persona']->loginname?></a></li><li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/logout">Salir</a></li>
-        <?php else:?>
-        	<li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/registrar">Registro</a></li><li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/login">Login</a></li>
-        <?php endif;?>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
-<?php if (isset ($datosGen['persona'])): ?>  
-<div class="area"></div>
+<?php if (isset ($datosGen['persona'])): ?> 
 <nav class="main-menu">
             <ul>
                 <li>
@@ -90,6 +46,51 @@
                     </a>
                 </li>
                 <li>
+                </li>
+                </ul>
+                
         </nav>
 <?php endif;?>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    <a   class="navbar-left" href="<?=base_url()?>"> 
+    <span><img src="<?=base_url()?>assets/img/iconotc.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+ 	</a>
+  
+   <a class="navbar-brand" href="<?=base_url()?>"> 
+    <span class="logoTakeCare textoLogo">TakeCare</span>
+  </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a class="textoexp2" href="#">Diagnóstico Online</a></li>
+        <li><a class="textoexp2" href="<?=base_url()?>profesional/r">Profesionales</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a><button type="button" onclick="cambiarModo()">
+        	<span><i class="fas fa-lightbulb"></i></span>
+    	</button></a>
+    	</li>
+    	
+		
+         <?php if ($datosGen['persona']!=null):?>
+        	<li><a class="textoexp1">Bienvenid@ <?=$datosGen['persona']->loginname?></a></li><li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/logout">Salir</a></li>
+        <?php else:?>
+        	<li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/registrar">Registro</a></li><li><a class="textoexp1" href="<?=base_url()?>hdu/anonymous/login">Login</a></li>
+        <?php endif;?>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
