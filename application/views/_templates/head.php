3834b5b0275763a8d0a0b3e1c134757a7f570796
NoelRodriguez11 <noelrodriguez2a@gmail.com>
2020-10-12 19:52:35 +0200

    footer casi terminado

--- application/views/_templates/head.php
+++ application/views/_templates/head.php
@@ -32,10 +32,7 @@
 	<script type="text/javascript" src="<?=base_url()?>assets/js/nav.js"></script>
    
    <!-- Prueba para el footer -->
-   
-<!--    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
-<!-- 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
-<!-- 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 	<title>TakeCare TFG</title>
 </head>
