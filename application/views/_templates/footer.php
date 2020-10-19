41294c90e15b351acaba43184c446730d2ed8db1
Victor Rivera <victor.rivera4@gmail.com>
2020-10-19 19:02:53 +0200

    footer

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,5 +1,20 @@
-      <div class="pie"> 
-              <a href='#'><i class="fa fa-facebook fa-3x fa-fw"></i></a>
-              <a href='#'><i class="fa fa-twitter fa-3x fa-fw"></i></a>
-               <a href='#'><i class="fa fa-instagram fa-3x fa-fw"></i></a>
-      </div>
+<!-- Footer -->
+<footer>
+
+<div class="container">
+    <hr>
+        <div class="text-center center-block">
+            <p class="txt-railway">- Footer de prueba -</p>
+            <br />
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+
+<br>
+
+<br />
+
+
+</footer>
+<!-- Footer -->
