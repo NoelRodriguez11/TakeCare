e77c29aed25b4249b0c8511ea320d3629eb47621
Victor Rivera <victor.rivera4@gmail.com>
2020-10-25 21:46:54 +0100

    Probando otros banners y quitando el espacio en el Navbar

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -7,9 +7,9 @@
         <br>
             <p>- TakeCare 2020 -</p>
             <br />
-                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
-	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter fa-3x social"></i></a>
+	            <a href="https://www.instagram.com/bootsnipp"><i id="social-ig" class="fa fa-instagram fa-3x social"></i></a>
 	            <a href="mailto:takecaretfg@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
 
 <br>
@@ -18,4 +18,3 @@
 
 
 </footer>
-<!-- Footer -->
