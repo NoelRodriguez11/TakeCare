0a8f94e86c81c86948370fd10ee3e633cde7e41b
guind <guind@192.168.1.8>
2020-11-08 18:03:46 +0100

    Pagina principal y nav cambiados

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer style="background-color: #e7e7e7; color:black;">
+<footer style="background-color: #e7e7e7; color:black;z-index:4;position:relative;">
 
 <div class="container">
 
@@ -7,8 +7,8 @@
         <br>
             <p>- TakeCare 2020 -</p>
             <br />
-                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook fa-3x social"></i></a>
-	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter fa-3x social"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100057232210381"><i id="social-fb" class="fa fa-facebook fa-3x social"></i></a>
+	            <a href="https://twitter.com/TakeCareTFG"><i id="social-tw" class="fa fa-twitter fa-3x social"></i></a>
 	            <a href="https://www.instagram.com/bootsnipp"><i id="social-ig" class="fa fa-instagram fa-3x social"></i></a>
 	            <a href="mailto:takecaretfg@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
 
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,120 +1,4 @@
 	<?php if ($rol == 'auth'):?>
-	<div class="container" id="divContenido">
-	<div class="contenidoPrincipal">
-		<div class="col-lg-6" id="buscador">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Search for...">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="button">Go!</button>
-					</span>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="acerca">
-    <h2>Contacta a los mejores profesionales en tu ciudad</h2>
-    
-    <div class="contenidoExplicativo">
-                    
-        <div class="primeraExplicacion">
-            <span class="logo1">LOGO1</span>
-            <strong><a class="textoexp1" href="">Profesores particulares</a></strong>
-            <br> 
-            <span class="textoexp2">anuncios de profesores particulares que ofrecen clases particulares en su casa o a domicilio.</span>
-        </div>
-        
-        <br>
-        
-        <div class="segundaExplicacion">
-            <span class="logo2">LOGO2</span>
-            <a class="textoexp1" href=""><strong>Clases a domicilio</strong></a> 
-            <br> 
-            <span class="textoexp2">encuentra el profesor particular ideal para recibir clases particulares a domicilio, cómodamente sin salir de casa.</span>
-        </div>
-        
-        <br>
-        
-        <div class="terceraExplicacion">
-            <span class="logo3">LOGO3</span>
-            <a class="textoexp1" href="">Academias</a>
-            <br>
-            <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-
-     </div>
-</div>
-	
-	
-	
-	<?php elseif ($rol == 'admin'): ?>
-	<div class="container" id="divContenido">
-	<div class="contenidoPrincipal">
-		<div class="col-lg-6" id="buscador">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Search for...">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="button">Go!</button>
-					</span>
-			</div>
-		</div>
-	</div>
-</div>
-
-<div class="acerca">
-    <h2>Contacta a los mejores profesionales en tu ciudad</h2>
-    
-    <div class="contenidoExplicativo">
-                    
-        <div class="primeraExplicacion">
-            <span class="logo1">LOGO1</span>
-            <strong><a class="textoexp1" href="">Profesores particulares</a></strong>
-            <br> 
-            <span class="textoexp2">anuncios de profesores particulares que ofrecen clases particulares en su casa o a domicilio.</span>
-        </div>
-        
-        <br>
-        
-        <div class="segundaExplicacion">
-            <span class="logo2">LOGO2</span>
-            <a class="textoexp1" href=""><strong>Clases a domicilio</strong></a> 
-            <br> 
-            <span class="textoexp2">encuentra el profesor particular ideal para recibir clases particulares a domicilio, cómodamente sin salir de casa.</span>
-        </div>
-        
-        <br>
-        
-        <div class="terceraExplicacion">
-            <span class="logo3">LOGO3</span>
-            <a class="textoexp1" href="">Academias</a>
-            <br>
-            <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-
-     </div>
-</div>
-	
-    	
-    	
-	<?php else: ?>
 <div  id="divContenido">
 	<div class="contenidoPrincipal">
 		<div class="col-lg-4" id="buscador">
@@ -131,123 +15,154 @@
 <div class="acerca">
     <h2>Contacta a los mejores profesionales en tu ciudad</h2>
     
-    <div class="contenidoExplicativo">
+    <div class="container" id="contenidoExplicativo">
                     
-        <div class="primeraExplicacion">
-            <span class="logo1">LOGO1</span>
-            <strong><a class="textoexp1" href="">Profesores particulares</a></strong>
+        <div class="explicacion">
+            <span class="logo1"><img src="<?=base_url()?>assets/img/acuerdo.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Contacto con profesionales vía web</span></strong>
             <br> 
-            <span class="textoexp2">anuncios de profesores particulares que ofrecen clases particulares en su casa o a domicilio.</span>
+            <span class="textoexp2">Contacto directo con profesionales via chat</span>
         </div>
         
         <br>
         
-        <div class="segundaExplicacion">
-            <span class="logo2">LOGO2</span>
-            <a class="textoexp1" href=""><strong>Clases a domicilio</strong></a> 
+        <div class="explicacion">
+            <span class="logo2"><img src="<?=base_url()?>assets/img/farmacia.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
             <br> 
-            <span class="textoexp2">encuentra el profesor particular ideal para recibir clases particulares a domicilio, cómodamente sin salir de casa.</span>
+            <span class="textoexp2">encuentra el profesional ideal para cubrir tu tratamiento</span>
         </div>
         
         <br>
         
-        <div class="terceraExplicacion">
-            <span class="logo3">LOGO3</span>
-            <a class="textoexp1" href="">Academias</a>
+        <div class="explicacion">
+            <span class="logo3"><img src="<?=base_url()?>assets/img/corazon.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
             <br>
             <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
         </div>
         
         <br>
         
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
+        <div class="explicacion">
+            <span class="logo4"><img src="<?=base_url()?>assets/img/medicina.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
             <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
+            <span class="textoexp2"></span>
         </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
-        
-        <br>
-        
-        <div class="cuartaExplicacion">
-            <span class="logo4">LOGO4</span>
-            <a class="textoexp1" href="">Clases para empresas</a>
-            <br> 
-            <span class="textoexp2">formación para empresas y autónomos (in company), impartidas por los mejores formadores, pudiendo ser subvencionadas o bonificadas.</span>
-        </div>
+     </div>
+</div> 
+	<?php elseif ($rol == 'admin'): ?>
+	
+<div  id="divContenido">
+	<div class="contenidoPrincipal">
+		<div class="col-lg-4" id="buscador">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search for...">
+					<span class="input-group-btn">
+						<button class="btn btn-default" type="button">Go!</button>
+					</span>
+			</div>
+		</div>
+	</div>
+</div>
 
+<div class="acerca">
+    <h2>Contacta a los mejores profesionales en tu ciudad</h2>
+    
+    <div class="container" id="contenidoExplicativo">
+                    
+        <div class="explicacion">
+            <span class="logo1"><img src="<?=base_url()?>assets/img/acuerdo.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Contacto con profesionales vía web</span></strong>
+            <br> 
+            <span class="textoexp2">Contacto directo con profesionales via chat</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo2"><img src="<?=base_url()?>assets/img/farmacia.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br> 
+            <span class="textoexp2">encuentra el profesional ideal para cubrir tu tratamiento</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo3"><img src="<?=base_url()?>assets/img/corazon.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br>
+            <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo4"><img src="<?=base_url()?>assets/img/medicina.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br> 
+            <span class="textoexp2"></span>
+        </div>
+     </div>
+</div> 
+	
+    	
+    	
+	<?php else: ?>
+	
+<div  id="divContenido">
+	<div class="contenidoPrincipal">
+		<div class="col-lg-4" id="buscador">
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Search for...">
+					<span class="input-group-btn">
+						<button class="btn btn-default" type="button">Go!</button>
+					</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="acerca">
+    <h2>Contacta a los mejores profesionales en tu ciudad</h2>
+    
+    <div class="container" id="contenidoExplicativo">
+                    
+        <div class="explicacion">
+            <span class="logo1"><img src="<?=base_url()?>assets/img/acuerdo.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Contacto con profesionales vía web</span></strong>
+            <br> 
+            <span class="textoexp2">Contacto directo con profesionales via chat</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo2"><img src="<?=base_url()?>assets/img/farmacia.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br> 
+            <span class="textoexp2">encuentra el profesional ideal para cubrir tu tratamiento</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo3"><img src="<?=base_url()?>assets/img/corazon.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br>
+            <span class="textoexp2">clases y cursos impartidas en academias, centros de formación y escuelas de idiomas.</span>
+        </div>
+        
+        <br>
+        
+        <div class="explicacion">
+            <span class="logo4"><img src="<?=base_url()?>assets/img/medicina.png" alt="INICIO" style="width: 50px; height:50px;"></span>
+            <strong><span class="textoexp1">Encuentra a los mejores profesionales de tu zona</span></strong>
+            <br> 
+            <span class="textoexp2"></span>
+        </div>
      </div>
 </div>  
 	<?php endif;?>
