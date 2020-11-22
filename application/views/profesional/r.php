cc07691ecd398d8e385445761e406bdbfcc3f1b6
Victor Rivera <victor.rivera4@gmail.com>
2020-11-22 17:40:10 +0100

    cambios

--- application/views/profesional/r.php
+++ application/views/profesional/r.php
@@ -12,28 +12,27 @@
 
 <div class="divAnuncioProfesionales row">
     	<div class="col-sm-11" id="tituloAnuncios">Anuncio ejemplo 1</div>
-        <div class="col-sm-1">
-        <div class="divEstrellitas">
-        <form>
-  			<p class="clasificacion" id="star0">
-  				<label for="radio1" id="radio0_5" class ="star" onClick="pulsarStar(this.id,1);">★</label>
-    				<input type="radio" name="estrellas" value="5">
-    			<label for="radio2" id="radio0_4" class ="star" onClick="pulsarStar(this.id,1);">★</label>
-   					<input type="radio" name="estrellas" value="4">
-                <label for="radio3" id="radio0_3" class ="star" onClick="pulsarStar(this.id,1);">★</label>
-                	<input type="radio" name="estrellas" value="3">
-                <label for="radio4"  id="radio0_2" class ="star" onClick="pulsarStar(this.id,1);">★</label>
-                	<input type="radio" name="estrellas" value="2">
-                <label for="radio5" id="radio0_1" class ="star" onClick="pulsarStar(this.id,1);">★</label>
-                	<input type="radio" name="estrellas" value="1">
-       			</p>	
-		</form>
-        </div>
+        <div class="col-sm-1">       
         <?php if ($datosGen['persona']==null):?>
         <button class="botonPedirCita btn btn-primary" id="botonPC" disabled>Pedir cita</button>
         <?php endif;?>
-    </div>
+   		</div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- El 1 es el id del profesional -->
 <div class="divAnuncioProfesionales row">
     <div class="col-sm-11" id="tituloAnuncios">Anuncio ejemplo 2</div>
