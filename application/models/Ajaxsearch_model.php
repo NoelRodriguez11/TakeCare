155ff507d2aa76fd82f08434c31054ead16b6e26
guind <noelrodriguez2a@gmail.com>
2020-11-25 22:23:12 +0100

    Cambios DatosGen y Añadidos 2 profesionales

---
+++ application/models/Ajaxsearch_model.php
@@ -0,0 +1,8 @@
+// <?php
+// class Ajaxsearch_model extends CI_Model
+// {
+//     function buscar($palabra = null) {
+//         return R::findAll('profesional', 'nombre LIKE "%'.$palabra.')
+//     }
+// }
+// ?>
