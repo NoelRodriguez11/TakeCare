b743af3885d752b3c750ea179bc0c1f080c3c2ba
lauraa09 <lauraa09>
2020-12-12 16:26:23 +0100

    css aviso y quieen

--- application/views/home/quienesSomos.php
+++ application/views/home/quienesSomos.php
@@ -6,11 +6,11 @@
 				<div class="col-md-12 mx-auto">
 					<br>
 					<h3>
-						<b>¿Quiénes somos?</b>
+						<b class="textoexp2">¿Quiénes somos?</b>
 					</h3>
 					<br>
 
-					<p>Take Care nace en el año 2020 como proyecto de fin de
+					<p class="textoexp2">Take Care nace en el año 2020 como proyecto de fin de
 						curso del Ciclo Formativo de Grado Superior en Desarrollo de
 						Aplicaciones Web impartido en el IES Rey Fernando VI, con la
 						finalidad exclusiva de poner en contacto a profesionales autónomos y 
@@ -20,7 +20,7 @@
 </p>
 
 
-					<p>Han sido muchas herramientas y tecnologías utilizadas
+					<p class="textoexp2">Han sido muchas herramientas y tecnologías utilizadas
 						para el desarrollo y la finalización del sitio web. Estas,
 						se han utilizado en base a los requerimientos del mismo, y las
 						enseñanzas impartidas por los profesores a lo largo de
@@ -29,14 +29,14 @@
 						para la finalización del proyecto.
 					
 					
-					</p><p>Desde Take Care queremos que los usuarios disfruten de la
+					</p><p class="textoexp2">Desde Take Care queremos que los usuarios disfruten de la
 						experiencia y repitan el uso de la aplicación que les
 						proporcionamos. Deseamos que el esfuerzo realizado por los
 						componentes del equipo de proyecto dé como fruto una gran
 						trayectoria profesional y futuras colaboraciones entre todas las
 						personas que nos han acompañado durante todo este tiempo.</p>
 
-					<p>Muchas gracias de parte del equipo de Take Care.</p>
+					<p class="textoexp2">Muchas gracias de parte del equipo de Take Care.</p>
 
 
 					<br>
